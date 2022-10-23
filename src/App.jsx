917df02e45eeafe362f3a218b19333b1fc5347a0
Satish Yadav <satish.yadav@nagarro.com>
2022-10-23 21:07:30 +0530

    component files extension changed to jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MediaSearch from './containers/media-search/media-search.container';
+
+const App = () => (
+	<>
+		{/* <MediaSearch /> */}
+		HI
+	</>
+);
+
+export default App;
