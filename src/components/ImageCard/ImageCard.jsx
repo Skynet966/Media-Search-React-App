a883322553fbae779192f884d8102f3d8cc6ea50
Satish Yadav <satish.yadav@nagarro.com>
2022-10-25 21:41:38 +0530

    Basic design structure completed

---
+++ src/components/ImageCard/ImageCard.jsx
@@ -0,0 +1,11 @@
+const ImageCard = () => {
+	return (
+		<img
+			className='sm:w-96 h-auto rounded-lg shadow-xl dark:shadow-gray-800'
+			src='https://flowbite.com/docs/images/blog/image-1.jpg'
+			alt='test'
+		/>
+	);
+};
+
+export default ImageCard;
--- src/layouts/MainContainer/MainContainer.jsx
+++ src/layouts/MainContainer/MainContainer.jsx
@@ -1,3 +1,4 @@
+import ImageCard from '../../components/ImageCard/ImageCard';
 import SearchBar from '../../components/SearchBar/SearchBar';
 const MainContainer = () => {
 	return (
@@ -5,9 +6,15 @@ const MainContainer = () => {
 			<div className=''>
 				<SearchBar />
 			</div>
-			<section>
-                Result Container
-            </section>
+			<section className='py-10'>
+				<div className='flex flex-row flex-wrap gap-10 justify-evenly'>
+					{[...Array(24).keys()].map(() => (
+						<div>
+							<ImageCard />
+						</div>
+					))}
+				</div>
+			</section>
 		</div>
 	);
 };
